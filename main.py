17c4f1660cfe2db61081502dd01117025c20ee51
Nguyễn Đình Chính <nd.chysnh@gmail.com>
2024-10-26 13:12:46 +0700

    Updated base code

---
+++ main.py
@@ -0,0 +1,21 @@
+import constants
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as: {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(constants.Discord_API_KEY_bot)
